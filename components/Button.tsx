0d37433213c791c7ea41fccc3634b5b39aa538c8
darshanbajania <darshanbajania1999@gmail.com>
2024-01-02 01:44:04 +0530

    feat: new project form setup

---
+++ components/Button.tsx
@@ -0,0 +1,47 @@
+import Image from "next/image";
+import React, { MouseEventHandler } from "react";
+
+type ButtonProps = {
+  title: string;
+  type: "button" | "submit";
+  leftIcon?: string | null;
+  rightIcon?: string | null;
+  handleClick?: MouseEventHandler;
+  isSubmitting?: boolean;
+  bgColor?: string;
+  textColor?: string;
+};
+
+const Button = ({
+  title,
+  type,
+  leftIcon,
+  rightIcon,
+  handleClick,
+  isSubmitting,
+  bgColor,
+  textColor,
+}: ButtonProps) => {
+  return (
+    <button
+      type={type || "button"}
+      disabled={isSubmitting}
+      // bgcolor and text color needs to be added
+      className={`flexCenter gap-3 px-4 py-3 
+      ${textColor || "text-white"}
+      ${
+        isSubmitting ? "bg-black/50" : bgColor || "bg-primary-purple"
+      } rounded-xl text-sm font-medium max-md:w-full
+      `}
+      onClick={handleClick}
+    >
+      {leftIcon && <Image src={leftIcon} width={14} height={14} alt="left" />}
+      {title}
+      {rightIcon && (
+        <Image src={rightIcon} width={14} height={14} alt="right" />
+      )}
+    </button>
+  );
+};
+
+export default Button;
--- components/ProjectForm.tsx
+++ components/ProjectForm.tsx
@@ -1,7 +1,122 @@
-import React from "react";
+"use client";
+import React, { ChangeEvent, useState } from "react";
+import { SessionInterface } from "@/common.types";
+import Image from "next/image";
+import FormField from "./FormField";
+import { categoryFilters } from "@/constants";
+import CustomMenu from "./CustomMenu";
+import Button from "./Button";
 
-const ProjectForm = () => {
-  return <div>ProjectForm</div>;
+type ProjectFromProps = {
+  type: string;
+  session: SessionInterface;
+};
+
+const ProjectForm = ({ type, session }: ProjectFromProps) => {
+  const handleFormSubmit = (e: React.FormEvent) => {};
+  const handleChangeImage = (e: ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    const file = e.target.files?.[0];
+    if (!file) return;
+
+    if (!file.type.includes("image")) {
+      alert("Please upload an image file");
+    }
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+
+    reader.onload = () => {
+      const result = reader.result as string;
+      handleStateChange("image", result);
+    };
+  };
+  const handleStateChange = (fieldName: string, value: string) => {
+    setForm((prevState) => ({
+      ...prevState,
+      [fieldName]: value,
+    }));
+  };
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [form, setForm] = useState({
+    title: "",
+    description: "",
+    liveSiteUrl: "",
+    githubUrl: "",
+    category: "",
+    image: "",
+  });
+  return (
+    <form onSubmit={handleFormSubmit} className="flexStart form">
+      <div className="flexStart form_image-container">
+        <label htmlFor="post" className="flexCenter form_image-label">
+          {!form.image && "Choose a poster for your project"}
+        </label>
+        <input
+          id="image"
+          type="file"
+          accept="image/*"
+          required={type === "create" ? true : false}
+          className="form_image-input"
+          onChange={handleChangeImage}
+        />
+
+        {form.image && (
+          <Image
+            src={form?.image}
+            className="sm:p-10 object-contain z-20"
+            alt="project poster"
+            fill
+          />
+        )}
+      </div>
+      <FormField
+        title="Title"
+        state={form.title}
+        placeholder={"Flexible"}
+        setState={(value) => handleStateChange("title", value)}
+      />
+      <FormField
+        title="Description"
+        state={form.description}
+        placeholder={"Showcase remarkable developer projects"}
+        setState={(value) => handleStateChange("description", value)}
+      />
+      <FormField
+        type="url"
+        title="Website URL"
+        state={form.liveSiteUrl}
+        placeholder={"https://"}
+        setState={(value) => handleStateChange("liveSiteUrl", value)}
+      />
+      <FormField
+        type="url"
+        title="Github URL"
+        state={form.githubUrl}
+        placeholder={"https://"}
+        setState={(value) => handleStateChange("githubUrl", value)}
+      />
+
+      <CustomMenu
+        title="Category"
+        state={form.category}
+        filters={categoryFilters}
+        setState={(value) => handleStateChange("category", value)}
+      />
+
+      <div className="flexStart w-full">
+        <Button
+          title={
+            isSubmitting
+              ? `${type === "create" ? "Creating" : "Editing"}`
+              : `${type === "create" ? "Create" : "Edit"}`
+          }
+          type="submit"
+          leftIcon={isSubmitting ? "" : "/plus.svg"}
+          isSubmitting={isSubmitting}
+        />
+      </div>
+    </form>
+  );
 };
 
 export default ProjectForm;
